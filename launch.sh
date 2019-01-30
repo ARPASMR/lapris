8c0612b04e209bf1af9b5d3c26dcb63596100f07
mauromussin <m.mussin@arpalombardia.it>
2019-01-30 19:00:38 +0100

    modifica creazione fof

--- launch.sh
+++ launch.sh
@@ -24,6 +24,7 @@ do
    Rscript prisma_cumula.R ${yesterday}00 24 Allerta CODICE_IM > prisma_cumula_${yesterday}.log
    mv *.png img/
   fi
+  ls -L ./static/*.png > ./static/fof.txt
   find *.log -mtime +7 -exec rm {} \; 
   sleep 3600
 done
