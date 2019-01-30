e4d83478b1fab2aad1b7e64ececb8571a19da0f5
root <root@TESTV-DCKRSWRM2.arpa.local>
2019-01-30 15:28:17 +0000

    versione funzionante in test

--- launch.sh
+++ launch.sh
@@ -7,16 +7,23 @@
 S3CMD='s3cmd --config=config_minio.txt'
 ./launch_flash.sh & 
 #endless loop
-#while [ 1 ]
-#do
+while [ 1 ]
+do
+  if [ $(date +"%H") == "05" ]; then
+
    #list dei file di ieri
-   #yesterday=$(date -d "yesterday" +"%Y%m%d")
-   yesterday='20180528'
-   $S3CMD ls s3://prisma/dati/cumulata_oraria_prisma_${yesterday}*.txt > elenco.txt 
+   yesterday=$(date -d "yesterday" +"%Y%m%d")
+   #yesterday='20180528'
+   $S3CMD ls s3://prisma/dati/cumulata_oraria_prisma_${yesterday}* > elenco.txt 
    # copia i file di ieri nella cartella dati
    for i in $(cat elenco.txt |awk '{ print $4; }');
    do
      s3cmd --config=config_minio.txt --force get $i dati/
    done
-   Rscript prisma_cumula.R ${yesterday}00 24 Allerta CODICE_IM
-#done
+   #eseguo lo script reindirizzando nei log
+   Rscript prisma_cumula.R ${yesterday}00 24 Allerta CODICE_IM > prisma_cumula_${yesterday}.log
+   mv *.png img/
+  fi
+  find *.log -mtime +7 -exec rm {} \; 
+  sleep 3600
+done
--- mostra.py
+++ mostra.py
@@ -6,11 +6,7 @@ Created on Tue Oct  2 10:07:26 2018
 """
 from flask import Flask
 from flask import render_template
-#from flask_s3 import FlaskS3
 app = Flask(__name__)
-app.config['FLASKS3_BUCKET_NAME']='lapris'
-app.config['AWS_ACCESS_KEY_ID']='ACCESS_KEY'
-app.config['AWS_SECRET_ACCESS_KEY']='SECRET_KEY'
 
 #s3=FlaskS3(app)
 @app.route("/")
