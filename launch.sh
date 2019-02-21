05341a2c686943594156a736651d2eac7451243e
mauromussin <m.mussin@arpalombardia.it>
2019-02-21 09:58:35 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -6,6 +6,14 @@
 # 4. far vedere le immagini
 S3CMD='s3cmd --config=config_minio.txt'
 ./launch_flash.sh & 
+# copio i file presenti in minio (solo ultima settimana)
+$S3CMD ls s3://prisma/ > elenco.txt
+tail -n 8 elenco.txt > elenco1.txt
+for i in $(cat elenco1.txt |awk '{ print $4; }');
+   do
+     s3cmd --config=config_minio.txt --force get $i static/
+   done
+   
 #endless loop
 while [ 1 ]
 do
